00cc909c5481a1be14b5c1bc199cfa2a52492a09
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-02-21 08:14:32 -0300

    Index routes
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/routes/Invoices/InvoicesIndex/index.tsx
@@ -0,0 +1,11 @@
+export default function InvoicesIndex() {
+    return (
+        <>
+
+            <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+            </main>
+
+        </>
+    )
+}
---
+++ src/routes/Wlcome/index.tsx
@@ -0,0 +1,11 @@
+export default function Welcome() {
+    return (
+        <>
+
+            <main style={{ padding: "1rem" }}>
+                <h1>Bem vindo</h1>
+            </main>
+
+        </>
+    )
+}
